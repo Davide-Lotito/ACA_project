718cd48f88d9e2f11b5febeaf30b72b0300e9bcb
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-02-05 14:50:32 +0100

    ...

--- test1.sh
+++ test1.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-g++ rabin_karp.cpp;
-./a.out ./data/genome_no_new_lines.txt ./data/pat.txt;
+# remove newlines from genome, then search genome for gene using rabin-karp.
+cd ./data/
+python3 remove_nl.py genome.fna 
+cd ..
+g++ rabin_karp.cpp
+./a.out ./data/genome_no_new_lines.txt ./data/pat.txt
 
