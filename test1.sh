f073b08df74b7931c721753ac6aeaadd7d9daf9c
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-02-09 07:42:57 +0100

    removed old files...

--- test1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# remove newlines from genome, then search genome for gene using rabin-karp.
-cd ./data/
-python3 remove_nl.py genome.fna 
-cd ..
-g++ -std=c++11 boyer_moore.cpp
-./a.out ./data/genome_no_new_lines.txt ./data/pat.txt
-
