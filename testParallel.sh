78a639c6de2dd4f0d8b74a10591d0209f2cf9e7f
Davide-Lotito <davidepio99.lotito@gmail.com>
2022-02-11 15:00:07 +0100

    refactor

--- testParallel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# remove newlines from genome, then search genome for gene using rabin-karp.
-rm ./a.out
-cd ./data/
-python3 remove_nl.py genome.fna 
-cd ..
-g++ -pthread -std=c++11 ./parallel1.cpp
-./a.out ./data/genome_no_new_lines.txt ./data/pat.txt
--- tests/testParallel2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# remove newlines from genome, then search genome for gene using rabin-karp.
-clear
-rm ../a.out
-cd ../data/
-python3 remove_nl.py genome.fna 
-cd ..
-mpic++ -std=c++11 ./parallel2.cpp
-mpiexec -n 16 --oversubscribe ./a.out ./data/genome_no_new_lines.txt ./data/pat.txt  
