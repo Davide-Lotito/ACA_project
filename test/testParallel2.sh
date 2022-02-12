ec702aab1d8bb6592e6aeeb260412e64d9f2e2e5
Davide-Lotito <davidepio99.lotito@gmail.com>
2022-02-12 11:47:48 +0100

    better split char

--- test/testParallel2.sh
+++ test/testParallel2.sh
@@ -4,6 +4,7 @@ clear
 rm ../a.out
 cd ../data/
 python3 remove_nl.py genome.fna 
+python3 remove_nl.py pat.txt
 cd ..
 mpic++ -std=c++11 ./parallel2.cpp
-mpiexec -n 16 --oversubscribe ./a.out ./data/genome_no_new_lines.txt ./data/pat.txt  
+mpiexec -n 16 --oversubscribe ./a.out ./data/genome_no_new_lines.txt ./data/pat_no_new_lines.txt  
