0c2d8743690bc3ea93aa43e79baf456594007669
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-02-16 18:36:33 +0100

    works without tqdm, displaying warning

--- src/clean_data/remove_nl.py
+++ src/clean_data/remove_nl.py
@@ -1,19 +1,18 @@
 import sys
 import os
 
-try: from tqdm import tqdm
-except ModuleNotFoundError as e: print("Please run:\n'pip install tqdm'\nAnd try again."); exit()
-
-
-
 usage_string =  f"""{100*"*"}\nUSAGE of 'remove_nl.py':\npython3 remove_nl.py path/to/myfile_or_my_dir\nCreates a new file: 'myfile_or_my_dir.txt', totally devoid of '\\n' chars.\nIn case a directory is given as input, the text-files it contains will be aggregated.\n{100*"*"}"""
-
 
 class Colors:
     OKGREEN = '\033[92m' 
     ENDC = '\033[0m'
     BOLD = '\033[1m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
     UNDERLINE = '\033[4m'
+
+try: from tqdm import tqdm; wrapper = tqdm
+except ModuleNotFoundError as e: wrapper = lambda x:x; print(Colors.WARNING+"tqdm not found, progress-bar not available."+Colors.ENDC); 
 
 
 def chunker(seq, size):
@@ -41,7 +40,7 @@ def __main__():
     # read and eventually concatenate
     print("reading files...")
     buffer = ""
-    for f in tqdm(files):
+    for f in wrapper(files):
         with open(f, "r") as f:
             buffer += f.read()
     
@@ -54,7 +53,7 @@ def __main__():
         f.write("")
 
     with open( newname , "a+") as f:
-        for chunk in tqdm(chunker(buffer, CHUNK_SIZE)):
+        for chunk in wrapper(chunker(buffer, CHUNK_SIZE)):
             chunk = chunk.replace("\n", "") #remove any newline from each chunk
             f.write(chunk) 
     print(f"{Colors.OKGREEN}done: remove_nl.py{Colors.ENDC}")
