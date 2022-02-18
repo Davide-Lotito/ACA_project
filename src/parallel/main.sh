a9fe1024a960d12f63d0403e7e05da9a17278689
Davide-Lotito <davidepio99.lotito@gmail.com>
2022-02-18 11:29:54 +0000

    cluster data

--- src/parallel/main.sh
+++ src/parallel/main.sh
@@ -3,6 +3,6 @@
 clear
 rm a.out
 mpic++ -std=c++11 main.cpp
-mpiexec -n 16 --oversubscribe ./a.out ../../data/EscherichiaColi/genome.fna  ../../data/pat.txt
-# mpiexec -n $1  ./a.out ../../data/EscherichiaColi/genome.fna  ../../data/pat.txt
+#mpiexec -n 16 --oversubscribe ./a.out ../../data/EscherichiaColi/genome.fna  ../../data/pat.txt
+mpiexec -n $1  ./a.out ../../data/EscherichiaColi/genome.fna  ../../data/pat.txt
 rm a.out
--- src/performance_tests/scalability.sh
+++ src/performance_tests/scalability.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mpiexec  -np  $1 ./a.out $2  ../../data/pat.txt    
+mpirun --hostfile ../parallel/hostfile -np $1 ./a.out $2  ../../data/pat.txt    
